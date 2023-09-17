13ba80dc8ec1cae4e1fd158b362331dd7831833d
gpo1-utec-adc <144974697+gpo1-utec-adc@users.noreply.github.com>
2023-09-17 00:04:02 -0500

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "access_key" {
+  type    = string
+  default = "AKIAZHKADEG35IJVLRUZ"
+}
+
+variable "secret_key" {
+  type    = string
+  default = "Qm+6QTyCiWAmptK6YIvWZ50eaI4lwIZV7IHbjwOa"
+}
